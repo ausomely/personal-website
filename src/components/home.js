d45dde89b5b164a49c0ba6f21973bb97c4b1163b
ausomely <jaackieau@gmail.com>
2021-03-31 18:01:34 -0700

    more css

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ export default function Home() {
         <div className='home-container-wrapper'>
             <div className='home-container'>
                 <h1 className='home-title user-select-none'>Fullstack Developer<span>.</span></h1>
-                
+                <div className='box'></div>
             </div>
         </div>
     );
