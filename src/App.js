30974a75f86c90886062f77b7be57b8eb5ec6b64
ausomely <jaackieau@gmail.com>
2021-04-02 20:12:54 -0700

    added icon anchor

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react';
 import Navbar from './components/navbar.js';
 import Home from './components/home.js';
+import Logo from './components/logo';
 
 export default function App() {
 
     return (
         <div className = 'App'> 
+            <Logo/>
             <Navbar/>
             <Home/>
         </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,7 +10,7 @@ export default class Navbar extends React.Component {
                 <div className='navbar-container'>
                     <div className='element-container user-select-none'>
                         <div className='navbar-element'>ABOUT</div>
-                        <div className='navbar-element'>RESUME</div>
+                        <a className='navbar-element' href = {resume} target='_blank' rel='noreferrer'>RESUME</a>
                         <div className='navbar-element'>SKILLS</div>
                         <div className='navbar-element'>PORTFOLIO</div>
                         <div className='navbar-element'>CONTACT</div>
