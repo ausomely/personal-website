4aaa1698d120493f2bb1975e611791c447b5c063
ausomely <jaackieau@gmail.com>
2021-08-12 01:24:49 -0700

    iphone 6,7,8 media query complete

--- src/components/navbar-home.js
+++ src/components/navbar-home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-scroll';
+import resume from '../resume/JACKIE_AU_PLAIN.pdf';
 
 export default class NavbarHome extends Component {
     render() {
@@ -13,12 +14,46 @@ export default class NavbarHome extends Component {
                     <div>
                         <div>
                             <ul>
-                                <li><a href="#">HOME</a></li>
-                                <li><a href="#">ABOUT</a></li>
-                                <li><a href="#">RESUME</a></li>
-                                <li><a href="#">SKILLS</a></li>
-                                <li><a href="#">PORTFOLIO</a></li>
-                                <li><a href="#">CONTACT</a></li>
+                                <li><a className='navbar-element' href = "/"
+                                    target='_self' rel='noreferrer'>HOME</a></li>
+                                <li><Link
+                                    to='about-container-wrapper'
+                                    spy={true}
+                                    smooth={true}
+                                    offset={0}
+                                    duration={1000}
+                                >
+                                    <div className='navbar-element'>ABOUT</div>
+                                </Link></li>
+                                <li><a className='navbar-element' href = {resume} 
+                                    target='_blank' rel='noreferrer'>RESUME</a></li>
+                                <li><Link
+                                    to='skills-container-wrapper'
+                                    spy={true}
+                                    smooth={true}
+                                    offset={0}
+                                    duration={1000}
+                                >
+                                    <div className='navbar-element'>SKILLS</div>
+                                </Link></li>
+                                <li><Link
+                                    to='portfolio-container-wrapper'
+                                    spy={true}
+                                    smooth={true}
+                                    offset={0}
+                                    duration={1000}
+                                >
+                                    <div className='navbar-element'>PORTFOLIO</div>
+                                </Link></li>
+                                <li><Link
+                                    to='contact-container-wrapper'
+                                    spy={true}
+                                    smooth={true}
+                                    offset={0}
+                                    duration={1000}
+                                >
+                                    <div className='navbar-element'>CONTACT</div>
+                                </Link></li>
                             </ul>
                         </div>
                     </div>
